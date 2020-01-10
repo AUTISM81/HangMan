77f29841d2c0961047a565a2929d9cd079a77b3f
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2020-01-10 11:18:00 +0100

    Start of Christians Methods

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,3 +1,4 @@
 public class HangMan {
-
+    public static void main(String[] args) {
+    }
 }
--- src/methods_Christian.java
+++ src/methods_Christian.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class methods_Christian {
+    public static void antalspelare(String[] args) {
+        //scanner
+        Scanner kb = new Scanner(System.in);
+
+        //antalspelare val
+        System.out.println("Hur många spelare skulle du vela spela med?");
+
+        int antal = kb.nextInt();
+        
+    }
+
+    public static void ordval(String[] args) {
+
+    }
+
 }
