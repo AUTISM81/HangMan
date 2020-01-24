7db02af82612f7612e729533248ace6a25e77485
Jinxyz <xxyaymanyayxx@hotmail.com>
2020-01-24 10:56:30 +0100

    fixat grejor

--- src/methods_Rasmus.java
+++ src/methods_Rasmus.java
@@ -16,7 +16,111 @@ public class methods_Rasmus {
         try (Stream<String> lines = Files.lines(Paths.get("res/wordlist.md"))) {
             ord = lines.skip(randomInt).findFirst().get();
         }
-
         return ord;
+
+    }
+
+    public static void Gubbe() {
+        if (falskt = 1) {
+            System.out.println("===========");
+        }
+        else if (falskt = 2){
+            System.out.println("| \n"
+            + "| \n"
+            + "| \n"
+            + "| \n"
+            + "| \n"
+            + "| \n"
+            + "| \n"
+            + "=========== \n"
+            );
+
+        }
+        else if (falskt = 3){
+            System.out.println(" +-----+ \n"
+            + "        | \n"
+            + "        | \n"
+            + "        | \n"
+            + "        | \n"
+            + "        | \n"
+            + "        | \n"
+            + "=========== \n"
+            );
+
+        }
+        else if (falskt = 4){
+            System.out.println(" +-----+ \n"
+                    + " |    | \n"
+                    + " |    |\n"
+                    + "      |\n"
+                    + "      |\n"
+                    + "      |\n"
+                    + "      |\n"
+                    + "=========== \n"
+            );
+
+        }
+        else if (falskt = 5){
+            System.out.println(System.out.println(" +-----+ \n"
+                    + " |    | \n"
+                    + " |    |\n"
+                    + " o    |\n"
+                    + "      |\n"
+                    + "      |\n"
+                    + "      |\n"
+                    + "=========== \n");
+        }
+        else if (falskt = 6){
+            System.out.println(" +-----+ \n"
+                    + " |    | \n"
+                    + " |    |\n"
+                    + " o    |\n"
+                    + " |    |\n"
+                    + "      |\n"
+                    + "      |\n"
+                    + "=========== \n");
+        }
+        else if (falskt = 7){
+            System.out.println(" +-----+ \n"
+                    + " |    | \n"
+                    + " |    |\n"
+                    + " o    |\n"
+                    + " |    |\n"
+                    + "/     |\n"
+                    + "      |\n"
+                    + "=========== \n");
+
+        }
+        else if (falskt = 8){
+            System.out.println(" +-----+ \n"
+                    + " |    | \n"
+                    + " |    |\n"
+                    + " o    |\n"
+                    + " |    |\n"
+                    + "/ \\   |\n"
+                    + "      |\n"
+                    + "=========== \n");
+        }
+        else if (falskt = 9){
+            System.out.println(" +-----+ \n"
+                    + " |    | \n"
+                    + " |    |\n"
+                    + "\\o    |\n"
+                    + " |    |\n"
+                    + "/ \\   |\n"
+                    + "      |\n"
+                    + "=========== \n");
+
+        }
+        else if (falskt = 10) {
+            System.out.println(" +-----+ \n"
+                    + " |    | \n"
+                    + " |    |\n"
+                    + "\\o/    |\n"
+                    + " |    |\n"
+                    + "/ \\   |\n"
+                    + "      |\n"
+                    + "=========== \n");
+        }
     }
 }
