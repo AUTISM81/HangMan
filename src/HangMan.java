7fd02c790140e18f5d67cbe23b04dfd5186e1298
AUTISM81 <christian.backstrom@elev.ga.ntig.se>
2020-01-30 10:34:59 +0100

    Restart of game

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,20 +1,27 @@
 import java.io.IOException;
 
+//Det här programmet kör Hänga gubbe.
+
 public class HangMan {
     public static void main(String[] args) throws IOException {
-        String ord;
+        boolean game = true;
 
-        int antal = methods_Christian.antalspelare();
-        if (antal >= 2){
-            ord = methods_Christian.ordval();
-        } else {
-            ord = methods_Rasmus.Dator();
+        while (game == true) {
+            String ord;
+
+            int antal = methods_Christian.antalspelare();
+
+            if (antal >= 2) {
+                ord = methods_Christian.ordval();
+            } else {
+                ord = methods_Rasmus.Dator();
+            }
+
+            int resultat = methods_Christian.gissa_ordet(ord);
+
+            methods_Christian.results(resultat);
+            System.out.println("The word was:" + ord);
         }
-
-        int resultat = methods_Christian.gissa_ordet(ord);
-
-        methods_Christian.results(resultat);
-        System.out.println("The word was:" + ord);
 
 
     }
--- src/methods_Christian.java
+++ src/methods_Christian.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class methods_Christian {
     public static int antalspelare() {
+       /* Denna kod bestämmer hur många som ska spela ett spel.*/
+
         //scanner
         Scanner kb = new Scanner(System.in);
 
         //antalspelare val
         System.out.println("would you like to play one or two players?");
 
+        //val av antalspelare
         int antal = kb.nextInt();
 
         return antal;
     }
 
     public static String ordval() {
+        /*Det här är om det är en person som ska välja ett ord.*/
+
         //scanner
         Scanner kb = new Scanner(System.in);
 
@@ -23,6 +28,7 @@ public class methods_Christian {
         System.out.println("Write the word you would like your opponent to guess!");
         String ord = kb.nextLine();
 
+        //massvis med rader för att personen som ska gissa ordet inte ska kunna se detta ord.
         for (int i = 0; i < 100; i++){
             System.out.println();
         }
@@ -30,6 +36,8 @@ public class methods_Christian {
     }
 
     public static int gissa_ordet(String ord) {
+        /* Det här är koden som gör att personen kan gissa bokstäver. Denna person kan även gissa hela ordet och kunna få rätt för det.*/
+
         //scanner
         Scanner kb = new Scanner(System.in);
 
@@ -44,23 +52,25 @@ public class methods_Christian {
         char[] array = new char[längd];
         ord.toLowerCase();
 
+        //antalet bokstäver som är i ordet skrivet ut i bindesträck.
         for (int i = 0; i < array.length; i++){
             array[i] = '-';
         }
         System.out.println();
 
+        //koden som körs när personen ska gissa bokstäver eller hela ordet.
         while (falskt < 10 && correct < längd) {
             System.out.println("Guess a letter or the word");
             String gissning = kb.next();
             gissning.toLowerCase();
-            
+
             char bokstav = gissning.charAt(0);
 
             int indexC = check.indexOf(bokstav);
 
             int count = 0;
 
-
+            //Den här koden testar ifall gissningen var rätt eller inte.
             if (!gissning.equals(ord)) {
                 for (int i = 0; i < längd; i++) {
                     if (ord.charAt(i) == bokstav) {
@@ -78,6 +88,7 @@ public class methods_Christian {
                         correct++;
                         System.out.println("Your guess was correct, nice job!" + "\n");
 
+                        //den här for loopen byter ut din gissning ifall den var rätt i array så att du kan se alla dina rätta gissningar.
                         for (int i = 0; i < array.length; i++) {
                             if (bokstav == ord.charAt(i)) {
                                 array[i] = bokstav;
@@ -90,6 +101,7 @@ public class methods_Christian {
                         System.out.println("Your guess was incorrect, try again!" + "\n");
                         word += " " + bokstav;
 
+                        //printar ut ASCII version av en hängd gubbe rätt emot hur måpnga fel man har.
                         methods_Rasmus.Gubbe(falskt);
 
                         if (falskt == 1) {
@@ -103,18 +115,21 @@ public class methods_Christian {
                 check += bokstav;
                 System.out.println("guesses: " + word);
 
+                //skriver ut alla rätta gissningar och hur många bokstäver det är kvar i ordet som inte har gissats
                 System.out.print("word:");
                 for (int i = 0; i < array.length; i++) {
                     System.out.print(array[i]);
                 }
                 System.out.println();
             } else {
+                //ifall hela ordet har gissats så vinner man direkt.
                 correct = längd;
             }
         }
 
         int resultat;
 
+        //kollar om du har klarat av att gissa hela ordet och skickar variabel resultat till nästa metod.
         if (correct == längd)
             resultat = correct;
         else
@@ -125,9 +140,25 @@ public class methods_Christian {
 
     public static void results(int resultat) {
 
+
+        //skriver ut om du har vunnit eller inte.
         if (resultat > 0)
             System.out.println("You won the game!" + "\n" + "Congratulations!");
         else
             System.out.println("Game over" + "\n" + "Better luck next time!");
+    }
+
+    public static boolean Restart(){
+        //scanner
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("Would you like to play another game? Yes or no?");
+
+        String ans = kb.nextLine();
+        ans.toLowerCase();
+        if (!ans.equals("yes")){
+            game = false;
+        }
+        return  game;
     }
 }
